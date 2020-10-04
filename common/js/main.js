41b119de1fab6dc4d5ad1d91ab06ab0cb131e730
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-10-04 19:28:19 +0200

    Remove google fonts

--- common/js/main.js
+++ common/js/main.js
@@ -1,8 +1,4 @@
-const locationUrl = location.href.split("/");
-locationUrl.shift();
-locationUrl.shift();
-
 const providerFields = document.getElementsByClassName("provider");
 for (var i = 0; i < providerFields.length; i++) {
-  providerFields[i].innerHTML = locationUrl[0].split(".")[0].toLowerCase();
+  providerFields[i].innerHTML = location.host;
 }
